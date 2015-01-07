a64947e31b8a4a1efa9dcdf14a8cb0678f97c0d3
satuday <felix_zhang@yahoo.com>
2015-01-07 16:53:47 -0500

    fix for unicode file name

--- checkFile.py
+++ checkFile.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+from __future__ import unicode_literals
 import os
 from datetime import datetime
 from datetime import timedelta
@@ -8,22 +10,20 @@ import time
 import shutil
 import sys
 
+
 dest_dir = "C:\\1\\"
 dl_dir = "Z:\\Download\\"
 bc_cmd = "C:\\Program Files\\BitComet\\BitComet.exe {0} -o {1} -s"
 
 def checkFiles(folderPath):
+	#shutil.move(unicode("C:\\[bbs[1][1].ysk.cc]足球小將世青賽全集13集全.txt", 'utf-8'), unicode("C:\\1\\[bbs[1][1].ysk.cc]足球小將世青賽全集13集全.txt", 'utf-8'))
 	for btfile in glob.iglob(folderPath + "*.txt"): #.torrent
-		dest_file = unicode(dest_dir + os.path.basename(btfile), sys.getfilesystemencoding())
-		uni_file = unicode(btfile, sys.getfilesystemencoding())
-		print uni_file, dest_file, 
-		print (bc_cmd.format(dest_file, dl_dir))
+		dest_file = dest_dir + os.path.basename(btfile)
 		if os.path.exists(dest_file):
 			os.remove(dest_file)
-		shutil.copy2(uni_file, dest_dir)
-		#print (bc_cmd.format(dest_file, dl_dir)) #os.startfile(dest_file) 
-
-
+		shutil.move(btfile, dest_dir)
+		os.startfile(bc_cmd.format(dest_file, dl_dir)) 
+		#os.startfile(dest_file) 
 
 def longRunningProcess():
 	while True:
