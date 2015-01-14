78992e713b837c64e371a0d405e78e507ba6f806
Felix <satuday@aim.com>
2015-01-13 20:59:32 -0500

    add .bat with change code page for print unicode char in console. rename file

---
+++ btLongRunningProcess.bat
@@ -0,0 +1,2 @@
+chcp 65001
+E:\btProcess.py
--- btProcess.py
+++ btProcess.py
@@ -10,32 +10,38 @@ import time
 import shutil
 import sys
 
-processed_dir = "C:\\1\\"
+processed_dir = "E:\\Processed\\"
 dl_dir = "Z:\\Download\\"
 bc_cmd = "C:\\Program Files\\BitComet\\BitComet.exe"
-rar_cmd = "C:\\Program Files\\WinRAR\\Rar.exe"
+rar_cmd = "C:\\Program Files\\WinRAR\\WinRar.exe"
 
 def uncompress(folderPath):
-	for rarfile in glob.iglob(folderPath + "*.rar" ):
-		print(rarfile)
-		args = [rar_cmd, "e", rarfile, "*.txt", "C:\\"]
+	for rarfile in glob.iglob(folderPath + "*.zip" ):
+		args = [rar_cmd, "e", rarfile, "*.torrent", "E:\\"]
+		print(args)
 		subprocess.call(args)
 		os.remove(rarfile)
-
+		
+	for rarfile in glob.iglob(folderPath + "*.rar" ):
+		args = [rar_cmd, "e", rarfile, "*.torrent", "E:\\"]
+		print(args)
+		subprocess.call(args)
+		os.remove(rarfile)
+		
 def checkFiles(folderPath):
 	uncompress(folderPath)
-	for btfile in glob.iglob(folderPath + "*.txt"): #.torrent
+	for btfile in glob.iglob(folderPath + "*.torrent"): #.torrent
 		dest_file = processed_dir + os.path.basename(btfile)
+		print(dest_file)
 		if os.path.exists(dest_file):
 			os.remove(dest_file)
 		shutil.move(btfile, processed_dir)
-		args = [bc_cmd, dest_file, '-o ' + processed_dir, '-s']
-		print("done")
-		#subprocess.Popen(args) 
+		args = [bc_cmd, dest_file, '-o ' + dl_dir, '-s']
+		subprocess.Popen(args) 
 
 def longRunningProcess():
 	while True:
-		checkFiles("C:\\")
+		checkFiles("E:\\")
 		time.sleep(5)
 
 
